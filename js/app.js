df1a251e7a80dd6eed5dc639f117dd1da444dac0
Kamil <kamil.muzyka91@gmail.com>
2021-02-10 00:24:19 +0100

    add smooth scroling

--- js/app.js
+++ js/app.js
@@ -4,6 +4,11 @@ function scrollToSection(el) {
   el.scrollIntoView();
 }
 
+// zamień funkcję na zdarzenie, bo nie przekazujesz żadnych parametrów 
+btn.addEventListener("click", function(){
+  window.scrollTo(0,200);
+})
+
 // scroll with the up arrow
 function scrollUp() {
   return window.scrollTo({
